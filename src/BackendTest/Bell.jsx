ae0caefe38c3a2becad9b645a7343f42df30c919
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-05-19 00:55:14 +0100

    updated

--- src/BackendTest/Bell.jsx
+++ src/BackendTest/Bell.jsx
@@ -14,7 +14,7 @@ function Bell  ( {notification, connected, onClear} ) {
             <div className="relative inline-block ">
                     <button 
                     onClick={ handleClick}
-                    className=" text-2xl bg-none border-none cursor-pointer relative ">
+                    className=" text-2xl bg-none border-none cursor-pointer relative ">  🔔
                         {
                             unreadCount > 0 && (
                                 <span className=" absolute -top-4 -right-4 bg-red-500 text-white rounded-full text-2 w-18 h-18 flex align-center justify-center ">
@@ -25,7 +25,10 @@ function Bell  ( {notification, connected, onClear} ) {
                     </button>
                     { open && (
                         <div className=" absolute right-0 top-9 w-7xl max-h-400 overflow-y-auto bg-gray-900 border rounded-8 p-3.5 border-blue-900 z-1000 shadow-2xl shadow-indigo-500 "> 
-                            ddfddssd
+                            <div className="  ">
+                                
+                                  </div>
+
                         </div>
                     ) }
 
