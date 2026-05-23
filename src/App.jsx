2f1ba12afc207c7020d2eb226e568fc74e657335
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-05-23 07:38:45 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
 import { useState } from 'react'
 import UserCard from './UserCard';
 import UsestateLearn from './learnings/UsestateLearn';
+import SocketConnection from "/src/Services/SocketConnection.jsx"
+import Bell from './BackendTest/Bell'; 
+import InfoInput from './BackendTest/InfoInput';
 
 
 function App() {
 // this is where you write variable and other js code to be returned
 
  const name = "dev tony";
+  const { notifications, connected, clearNotification } = SocketConnection (
+    "http://localhost:8080/ws",      // your Spring Boot base URL
+    "/topic/notifications"           // the topic to subscribe to
+  );
+
   return (
     
-      <div className="h-screen  gap-7 justify-center mt-10">
-        <UserCard name="dev Tony" id={234} />
-        <UserCard name="dev Tony" id={23} />
-        <UserCard name="dev Tony" id={2} />
-        <UsestateLearn />
+      <div className=" p-8 font-serif">
+      <nav className="  flex justify-between p-4 bg-gray-950 text-white rounde-8 mb-8 align-center  ">
+      <span className=' font-bold text-2xl '>
+        My App
+      </span>
+      <Bell notifications={notifications} connected={connected } onClear={clearNotification} />
+      </nav>
+
+      <h2>Dashboard</h2>
+      <p className=' text-white' > notification will appear on the bell in real time</p>
+      <InfoInput />
+       
       </div>
   )
 }
